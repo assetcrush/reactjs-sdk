fa26be8962e1ff055ce1e416572b08ea8ad4b6b8
ali-718 <alimurtuza718@gmail.com>
2022-01-06 22:04:41 +0500

    added ref

--- src/ImageResize.jsx
+++ src/ImageResize.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 
 export const ImageResize = ({ url, width, height, key }) => {
-  const imageUrl = `https://service.assetcrush.com?width=${width}&height=${height}&original_uri=${url}`;
-  return <img src={imageUrl} alt={url} />;
+  const targetRef = useRef();
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+  useLayoutEffect(() => {
+    if (targetRef.current) {
+      setDimensions({
+        width: targetRef.current.offsetWidth,
+        height: targetRef.current.offsetHeight,
+      });
+    }
+  }, []);
+
+  const imageUrl = `https://service.assetcrush.com?width=${dimensions?.width}&height=${dimensions.height}&original_uri=${url}`;
+  return (
+    <div ref={targetRef}>
+      <img ref={targetRef} src={imageUrl} alt={url} />
+    </div>
+  );
 };
