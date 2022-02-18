b0238cef21aeb05c69ef7423e1c9a6428ce14a37
assetcrush <dev@assetcrush.com>
2022-02-19 01:36:47 +0500

    refactor sdk

--- example/src/App.js
+++ example/src/App.js
@@ -1,18 +1,18 @@
-import React from "react";
-import ImageResize from "./ImageResize";
-import { setKey } from "./key";
+import React from 'react';
+import ImageResize from './ImageResize';
+import { setKey } from './key';
 
-setKey("123");
+setKey('test-key');
+
+const testUrl = 'https://cdn.pixabay.com/photo/2021/12/11/07/59/hotel-6862159__340.jpg'
 
 function App() {
   return (
     <div style={{ width: 50, height: 50 }}>
       <ImageResize
-        url={
-          "https://cdn.pixabay.com/photo/2021/12/11/07/59/hotel-6862159__340.jpg"
-        }
-        width={"100"}
-        height={"100"}
+        url={testUrl}
+        width={200}
+        height={200}
       />
     </div>
   );
