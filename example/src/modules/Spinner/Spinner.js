16df6f2c9a0f9bb5e5855463a59fe88593bd388e
Ali Haider <alimurtuza718@gmail.com>
2022-04-27 21:09:54 +0500

    Size (#7)
    * fixed the size of loading icon
    
    * add header and height constraint
    
    * updated version
    
    * spinner size fixed

--- example/src/modules/Spinner/Spinner.js
+++ example/src/modules/Spinner/Spinner.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./styles.css";
 
-export const Spinner = ({icon:Icon, backgroundColor = 'gray'}) => {
+export const Spinner = ({
+  icon: Icon,
+  backgroundColor = "gray",
+  width,
+  height,
+}) => {
   if (Icon) return Icon;
   return (
     <>
-    <style>
-      {`
+      <style>
+        {`
       .assetcrush-lds-roller div:after {
         content: " ";
         display: block;
@@ -18,17 +23,30 @@ export const Spinner = ({icon:Icon, backgroundColor = 'gray'}) => {
         margin: -4px 0 0 -4px;
       }
       `}
-    </style>
-    <div className="assetcrush-lds-roller">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
+      </style>
+      <div
+        style={{
+          transform: `${
+            width < height
+              ? `scale(${height > 100 ? 1 : height / 300},${
+                  height > 100 ? 1 : height / 300
+                })`
+              : `scale(${width > 100 ? 1 : width / 300},${
+                  width > 100 ? 1 : width / 300
+                })`
+          }`,
+        }}
+        className="assetcrush-lds-roller"
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </>
   );
 };
